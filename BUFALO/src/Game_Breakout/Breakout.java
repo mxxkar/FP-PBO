80ce1a1f5fd10fbdde229f37608ad3190b374c3c
Sekar <90591077+mxxkar@users.noreply.github.com>
2022-12-05 21:30:01 +0700

    Add files via upload

---
+++ BUFALO/src/Game_Breakout/Breakout.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Game_Breakout;
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+public class Breakout extends JFrame {
+
+    public Breakout() {
+        
+        initUI();
+    }
+    
+    private void initUI() {
+
+        add(new Board());
+        setTitle("Breakout");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        pack();
+    }
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+
+            var game = new Breakout();
+            game.setVisible(true);
+        });
+    }
+}
---
+++ BUFALO/src/Game_Breakout/Brick.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Game_Breakout;
+
+import javax.swing.ImageIcon;
+
+public class Brick extends Sprite {
+
+    private boolean destroyed;
+
+    public Brick(int x, int y) {
+        
+        initBrick(x, y);
+    }
+    
+    private void initBrick(int x, int y) {
+        
+        this.x = x;
+        this.y = y;
+        
+        destroyed = false;
+
+        loadImage();
+        getImageDimensions();
+    }
+    
+    private void loadImage() {
+        
+        var ii = new ImageIcon("src/resources/brick.png");
+        image = ii.getImage();        
+    }
+
+    boolean isDestroyed() {
+        
+        return destroyed;
+    }
+
+    void setDestroyed(boolean val) {
+        
+        destroyed = val;
+    }
+}
